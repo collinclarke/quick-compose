bd6a6861a92ce17420defaa9b6691bf51dea8b26
Collin Clarke <collinclarke@gmail.com>
2020-06-04 20:15:15 -0400

    --amend

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,12 @@ const path = require("path");
 
 module.exports = {
   outputDir: path.resolve(__dirname, "docs"),
+  chainWebpack: config => {
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = 'Auto Compose'
+      return args
+    })
+  }
 }
